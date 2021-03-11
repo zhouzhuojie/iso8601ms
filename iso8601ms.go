37fcf6b59f7b771e1bcb5843254536f292b57f7f
zhuojie <zhuojie@checkr.com>
2021-03-10 17:43:15 -0800

    Reuse time.Time unmarshal

--- iso8601ms.go
+++ iso8601ms.go
@@ -6,14 +6,16 @@ import (
 	"time"
 )
 
-const iso8601msFormat = "2006-01-02T15:04:05.000Z"
+const (
+	iso8601msFormat = "2006-01-02T15:04:05.000Z07:00"
+)
 
 // Time is a iso8601ms struct
 type Time time.Time
 
 // MarshalJSON implements the json.Marshaler interface.
 func (t Time) MarshalJSON() ([]byte, error) {
-	tt := time.Time(t).UTC()
+	tt := time.Time(t)
 	s := `"` + tt.Format(iso8601msFormat) + `"`
 	return []byte(s), nil
 }
@@ -21,13 +23,8 @@ func (t Time) MarshalJSON() ([]byte, error) {
 // UnmarshalJSON implements the json.Unmarshaler interface.
 // The time is expected to be a quoted string in RFC 3339 format.
 func (t *Time) UnmarshalJSON(data []byte) error {
-	// Ignore null, like in the main JSON package.
-	if string(data) == "null" {
-		return nil
-	}
-	// Fractional seconds are handled implicitly by Parse.
-	var err error
-	tt, err := time.Parse(`"`+iso8601msFormat+`"`, string(data))
-	*t = Time(tt.UTC())
+	var tt time.Time
+	err := tt.UnmarshalJSON(data)
+	*t = Time(tt)
 	return err
 }
--- iso8601ms_test.go
+++ iso8601ms_test.go
@@ -6,16 +6,6 @@ import (
 	"time"
 )
 
-var jsonTests = []struct {
-	time time.Time
-	json string
-}{
-	{time.Date(9999, 4, 12, 23, 20, 50, 520*1e6, time.UTC), `"9999-04-12T23:20:50.520Z"`},
-	{time.Date(1996, 12, 19, 16, 39, 57, 0, time.FixedZone("UTC-4", -4*60*60)), `"1996-12-19T20:39:57.000Z"`},
-	{time.Date(1996, 12, 19, 16, 39, 57, 1*1e6, time.FixedZone("UTC-7", -7*60*60)), `"1996-12-19T23:39:57.001Z"`},
-	{time.Date(0, 1, 1, 0, 0, 0, 0, time.UTC), `"0000-01-01T00:00:00.000Z"`},
-}
-
 func TestNow(t *testing.T) {
 	iso8601msTime := Time(time.Now())
 	jsonBytes, err := json.Marshal(iso8601msTime)
@@ -27,9 +17,67 @@ func TestNow(t *testing.T) {
 	}
 }
 
+func TestUnmarshal(t *testing.T) {
+	t.Run("valid cases", func(t *testing.T) {
+		var jsonIso8601msTime Time
+		var err error
+
+		err = json.Unmarshal([]byte(`"2021-03-11T01:29:34.317-07:00"`), &jsonIso8601msTime)
+		if err != nil {
+			t.Errorf("json.Unmarshal error = %v, want nil", err)
+		}
+
+		err = json.Unmarshal([]byte(`"2021-03-11T01:29:34.317Z"`), &jsonIso8601msTime)
+		if err != nil {
+			t.Errorf("json.Unmarshal error = %v, want nil", err)
+		}
+
+		err = json.Unmarshal([]byte(`"2021-03-11T01:29:34Z"`), &jsonIso8601msTime)
+		if err != nil {
+			t.Errorf("json.Unmarshal error = %v, want nil", err)
+		}
+
+		err = json.Unmarshal([]byte(`"2021-03-11T01:29:34.999999Z"`), &jsonIso8601msTime)
+		if err != nil {
+			t.Errorf("json.Unmarshal error = %v, want nil", err)
+		}
+
+		err = json.Unmarshal([]byte(`"2021-03-11T01:29:34.999999999Z"`), &jsonIso8601msTime)
+		if err != nil {
+			t.Errorf("json.Unmarshal error = %v, want nil", err)
+		}
+
+	})
+
+	t.Run("invalid cases", func(t *testing.T) {
+		var jsonIso8601msTime Time
+		var err error
+
+		err = json.Unmarshal([]byte(`"2021-03-11T01:29:34.317"`), &jsonIso8601msTime)
+		if err == nil {
+			t.Errorf("got nil error, want parsing error from json.Unmarshal")
+		}
+
+		err = json.Unmarshal([]byte(`"2021-03-11T01:29:34"`), &jsonIso8601msTime)
+		if err == nil {
+			t.Errorf("got nil error, want parsing error from json.Unmarshal")
+		}
+	})
+}
+
+var jsonTests = []struct {
+	time time.Time
+	json string
+}{
+	{time.Date(9999, 4, 12, 23, 20, 50, 520*1e6, time.UTC), `"9999-04-12T23:20:50.520Z"`},
+	{time.Date(1996, 12, 19, 16, 39, 57, 0, time.FixedZone("UTC-4", -4*60*60)), `"1996-12-19T16:39:57.000-04:00"`},
+	{time.Date(1996, 12, 19, 16, 39, 57, 1*1e6, time.FixedZone("UTC-7", -7*60*60)), `"1996-12-19T16:39:57.001-07:00"`},
+	{time.Date(0, 1, 1, 0, 0, 0, 0, time.UTC), `"0000-01-01T00:00:00.000Z"`},
+}
+
 func TestIso8601msJSON(t *testing.T) {
 	for _, tt := range jsonTests {
-		iso8601msTime := Time(tt.time.UTC())
+		iso8601msTime := Time(tt.time)
 		var jsonIso8601msTime Time
 
 		if jsonBytes, err := json.Marshal(iso8601msTime); err != nil {
@@ -38,15 +86,13 @@ func TestIso8601msJSON(t *testing.T) {
 			t.Errorf("%v JSON = %#q, want %#q", iso8601msTime, string(jsonBytes), tt.json)
 		} else if err = json.Unmarshal(jsonBytes, &jsonIso8601msTime); err != nil {
 			t.Errorf("%v json.Unmarshal error = %v, want nil", iso8601msTime, err)
-		} else if !equalTimeAndZone(jsonIso8601msTime, iso8601msTime) {
+		} else if !equalTime(jsonIso8601msTime, iso8601msTime) {
 			t.Errorf("Unmarshaled time = %v, want %v", jsonIso8601msTime, iso8601msTime)
 		}
 	}
 }
 
-func equalTimeAndZone(aa, bb Time) bool {
+func equalTime(aa, bb Time) bool {
 	a, b := time.Time(aa), time.Time(bb)
-	aname, aoffset := a.Zone()
-	bname, boffset := b.Zone()
-	return a.Equal(b) && aoffset == boffset && aname == bname
+	return a.Equal(b)
 }
